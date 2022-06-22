25e9f7084381549f136e48701c54ff7e444008e4
irfan2019 <sam20will@gmail.com>
2022-06-22 11:59:51 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React,{Component} from 'react';
 import  ReactDOM  from 'react-dom';
 //import Main from './components/Main';
 const tasks=['okok','koko','kookook'];
-
-
-
 class Lio extends Component
 {
   render()
@@ -39,40 +36,4 @@ class Wain extends Component
 }
 ReactDOM.render(<Wain/>,document.getElementById('root'));
 
-/*class List extends Component
-{
-  render()
-  {
-    return
-    (<ol>
-      {this.props.tasks.map((task,index) => <li key = {index} > {task} </li>)}
-    </ol>)
-  }
 
-}
-
-class Title extends Component
-{
-  render()
-  {
-    return
-    <h1> 
-      irfan
-      </h1> 
-  }
-}
-
-class Wain extends Component
-{
-  render()
-  {
-    return
-        <div>
-         <List  tasks={['okok','koko']}/>
-         <Title/>
-        </div>
-  }
-}*/
-
-
-//ReactDOM.render(<Wain/>,document.getElementById('root'));
